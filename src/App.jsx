afa864735ab2b6091d4c1cfd05666a40f1309ec7
Sebastian Galustyan <sebastian@7digit.io>
2022-11-05 13:14:58 +0000

    Add: QueryClientProvider and new route for PrivacyPolicy page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import {
   NotFound,
   ContactUs,
   NotificationPreferencesPage,
   Login,
+  PrivacyPolicy,
 } from "./pages";
 
 import "./App.scss";
+
+// Create a react-query client
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
 
 // AOS imports
 import "aos/dist/aos.css";
@@ -24,17 +31,20 @@ function App() {
   });
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/contact-us" element={<ContactUs />} />
-        <Route
-          path="/settings/notifications"
-          element={<NotificationPreferencesPage />}
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/contact-us" element={<ContactUs />} />
+          <Route
+            path="/settings/notifications"
+            element={<NotificationPreferencesPage />}
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
