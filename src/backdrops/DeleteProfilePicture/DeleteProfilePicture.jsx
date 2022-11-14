96bc56164c22e507568f13db88497fa3d84b6a56
Georgi 7DIGIT <georgi@7digit.io>
2022-11-14 17:16:00 +0200

    Create: DeleteProfilePicture backdrop

---
+++ src/backdrops/DeleteProfilePicture/DeleteProfilePicture.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Backdrop } from "@USupport-components-library/src";
+import { useError } from "@USupport-components-library/hooks";
+import { providerSvc } from "@USupport-components-library/services";
+
+import "./delete-profile-picture.scss";
+
+/**
+ * DeleteProfilePicture
+ *
+ * The DeleteProfilePicture backdrop
+ *
+ * @return {jsx}
+ */
+export const DeleteProfilePicture = ({ isOpen, onClose }) => {
+  const { t } = useTranslation("delete-profile-picture");
+  const [error, setError] = useState();
+  const queryClient = useQueryClient();
+
+  const deletePicture = async () => {
+    const res = await providerSvc.deleteImage();
+    if (res.status === 200) {
+      return true;
+    }
+  };
+
+  const deletePictureMutation = useMutation(deletePicture, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(["provider-data"]);
+      onClose();
+    },
+    onError: (error) => {
+      const { message: errorMessage } = useError(error);
+      setError(errorMessage);
+    },
+  });
+
+  const handlePictureDelete = () => deletePictureMutation.mutate();
+
+  return (
+    <Backdrop
+      classes="delete-profile-picture"
+      title="DeleteProfilePicture"
+      isOpen={isOpen}
+      onClose={onClose}
+      heading={t("heading")}
+      ctaLabel={t("remove_button")}
+      ctaHandleClick={handlePictureDelete}
+      secondaryCtaLabel={t("cancel_button")}
+      secondaryCtaHandleClick={onClose}
+      secondaryCtaType="secondary"
+      errorMessage={error}
+    >
+      <p className="delete-profile-picture__text">{t("text")}</p>
+    </Backdrop>
+  );
+};
