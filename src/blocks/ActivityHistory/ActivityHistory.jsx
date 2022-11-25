f142d057157d951654fdb588a88ab65e0236c975
Georgi 7DIGIT <georgi@7digit.io>
2022-11-25 04:08:26 +0200

    Refactor: QA review

--- src/blocks/ActivityHistory/ActivityHistory.jsx
+++ src/blocks/ActivityHistory/ActivityHistory.jsx
@@ -25,24 +25,34 @@ import "./activity-history.scss";
  *
  * @return {jsx}
  */
-export const ActivityHistory = () => {
+export const ActivityHistory = ({ openSelectConsultation }) => {
   const { t } = useTranslation("activity-history");
 
   const { width } = useWindowDimensions();
 
-  const [selectedConsultationId, setSelectedConsultationId] = useState("");
+  const [selectedConsultation, setSelectedConsultation] = useState("");
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const fetchConsultations = async () => {};
+
+  const daysOfWeekTranslations = {
+    monday: t("monday"),
+    tuesday: t("tuesday"),
+    wednesday: t("wednesday"),
+    thursday: t("thursday"),
+    friday: t("friday"),
+    saturday: t("saturday"),
+    sunday: t("sunday"),
+  };
 
   const consultationsQuery = useQuery(["consultations"], fetchConsultations, {
     enabled: false, // TODO: Enable this when the API is ready and remove the placeholder data
     placeholderData: [1, 2, 3, 4, 5, 6, 7, 8].map((x) => {
       return {
-        id: x,
+        providerId: "e04e0f50-6676-425d-997d-f790a030d7a3",
+        clientId: "225de85c-76b3-41db-8292-9ee27678f124",
         name: "Joanna Doe " + x.toString(),
-        startDate: new Date("2022-11-1 15:00"),
-        endDate: new Date("2022-11-1 16:00"),
+        timestamp: new Date().getTime() + x * 60 * 60 * 1000,
         overview: true,
       };
     }),
@@ -110,17 +120,19 @@ export const ActivityHistory = () => {
 
   const id = "1";
 
-  const handleConsultationClick = (id) => {
-    console.log(id);
-    setSelectedConsultationId(id);
+  const handleConsultationClick = (consultation) => {
+    window.scrollTo(0, 0);
+    setSelectedConsultation(consultation);
   };
 
   const handleGoBack = () => {
-    setSelectedConsultationId("");
+    window.scrollTo(0, 0);
+    setSelectedConsultation("");
   };
 
   const handleProposeConsultation = () => {
     setIsMenuOpen(false);
+    openSelectConsultation(selectedConsultation.clientId);
   };
 
   const renderMenuOptions = () => {
@@ -139,7 +151,11 @@ export const ActivityHistory = () => {
 
     return options.map((option, index) => {
       return (
-        <div className="menu-option" onClick={option.onClick} key={index}>
+        <div
+          className="menu-option"
+          onClick={option.onClick}
+          key={"menu-option" + index}
+        >
           <Icon
             name={option.iconName}
             color={"#373737"}
@@ -154,14 +170,15 @@ export const ActivityHistory = () => {
   const renderAllConsultations = () => {
     return consultationsQuery.data.map((consultation, index) => {
       return (
-        <GridItem key={index} md={8} lg={12}>
+        <GridItem key={"consultation-" + index} md={8} lg={12}>
           <Consultation
             name={consultation.name}
-            startDate={consultation.startDate}
-            endDate={consultation.endDate}
+            timestamp={consultation.timestamp}
             overview={consultation.overview}
             renderIn="client"
-            onClick={() => handleConsultationClick(consultation.id)}
+            onClick={() => handleConsultationClick(consultation)}
+            daysOfWeekTranslations={daysOfWeekTranslations}
+            providerId={consultation.providerId}
           />
         </GridItem>
       );
@@ -171,13 +188,31 @@ export const ActivityHistory = () => {
   const renderAllMessages = () => {
     return consultationsMessages.map((message) => {
       if (message.type === "system-message") {
-        return <SystemMessage title={message.message} date={message.date} />;
+        return (
+          <SystemMessage
+            key={message.date.getTime()}
+            title={message.message}
+            date={message.date}
+          />
+        );
       } else {
         if (message.senderId === id) {
-          return <Message message={message.message} sent date={message.date} />;
+          return (
+            <Message
+              key={message.date.getTime()}
+              message={message.message}
+              sent
+              date={message.date}
+            />
+          );
         } else {
           return (
-            <Message message={message.message} received date={message.date} />
+            <Message
+              key={message.date.getTime()}
+              message={message.message}
+              received
+              date={message.date}
+            />
           );
         }
       }
@@ -187,7 +222,7 @@ export const ActivityHistory = () => {
   return (
     <Block classes="activity-history">
       <div className="activity-history__content">
-        {((width < 1366 && !selectedConsultationId) || width >= 1366) && (
+        {((width < 1366 && !selectedConsultation) || width >= 1366) && (
           <div className="activity-history__main-container">
             <div className="activity-history__main-container__header">
               <h4>{t("activity_history_heading")}</h4>
@@ -204,9 +239,9 @@ export const ActivityHistory = () => {
             </Grid>
           </div>
         )}
-        {((width < 1366 && selectedConsultationId) || width >= 1366) && (
+        {((width < 1366 && selectedConsultation) || width >= 1366) && (
           <div className="activity-history__consultation-container">
-            {!selectedConsultationId ? (
+            {!selectedConsultation ? (
               <div className="activity-history__consultation-container__no-selected">
                 <img src={mascot} alt="Mascot" className="mascot" />
                 <h4 className="activity-history__consultation-container__no-selected__text">
@@ -226,7 +261,7 @@ export const ActivityHistory = () => {
                     )}
                     {/* TODO: refactor to display the name and image of the client  */}
                     <Avatar size="sm" />
-                    <h4 className="client-name">Joanna Doe </h4>
+                    <h4 className="client-name">{selectedConsultation.name}</h4>
                   </div>
                   <Icon
                     name="three-dots-vertical"
--- src/pages/ActivityHistory/ActivityHistory.jsx
+++ src/pages/ActivityHistory/ActivityHistory.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Page, ActivityHistory as ActivityHistoryBlock } from "#blocks";
+import { SelectConsultation } from "#backdrops";
+import { useBlockSlot, useSuggestConsultation } from "#hooks";
+import { userSvc } from "@USupport-components-library/services";
 
 import "./activity-history.scss";
 
@@ -11,6 +14,60 @@ import "./activity-history.scss";
  * @returns {JSX.Element}
  */
 export const ActivityHistory = () => {
+  const [selectedClientId, setSelectedClientId] = useState();
+  const [isSelectConsultationOpen, setIsSelectConsultationOpen] =
+    useState(false);
+
+  const [blockSlotError, setBlockSlotError] = useState();
+  const [isBlockSlotSubmitting, setIsBlockSlotSubmitting] = useState(false);
+
+  const closeSelectConsultation = () => setIsSelectConsultationOpen(false);
+
+  const providerId = userSvc.getUserID();
+
+  const openSelectConsultation = (clientId) => {
+    setSelectedClientId(clientId);
+    setIsSelectConsultationOpen(true);
+  };
+
+  const onSuggestConsultationSuccess = (data) => {
+    setIsBlockSlotSubmitting(false);
+    // setConsultationId(consultationId);
+    closeSelectConsultation();
+    openConfirmConsultationBackdrop();
+    setBlockSlotError(null);
+  };
+  const onSuggestConsultationError = (error) => {
+    setBlockSlotError(error);
+    setIsBlockSlotSubmitting(false);
+  };
+  const suggestConsultationMutation = useSuggestConsultation(
+    onSuggestConsultationSuccess,
+    onSuggestConsultationError
+  );
+
+  const onBlockSlotSuccess = (consultationId) => {
+    // setIsBlockSlotSubmitting(false);
+    // setConsultationId(consultationId);
+
+    suggestConsultationMutation.mutate(consultationId);
+    setIsBlockSlotSubmitting(false);
+    // closeSelectConsultation();
+    // openConfirmConsultationBackdrop();
+  };
+  const onBlockSlotError = (error) => {
+    setBlockSlotError(error);
+    setIsBlockSlotSubmitting(false);
+  };
+  const blockSlotMutation = useBlockSlot(onBlockSlotSuccess, onBlockSlotError);
+  const handleBlockSlot = (slot) => {
+    setIsBlockSlotSubmitting(true);
+    blockSlotMutation.mutate({
+      slot,
+      clientId: selectedClientId,
+    });
+  };
+
   return (
     <Page
       classes="page__activity-history"
@@ -18,7 +75,17 @@ export const ActivityHistory = () => {
       showFooter
       showGoBackArrow={false}
     >
-      <ActivityHistoryBlock />
+      <ActivityHistoryBlock openSelectConsultation={openSelectConsultation} />
+      <SelectConsultation
+        isOpen={isSelectConsultationOpen}
+        onClose={() => setIsSelectConsultationOpen(false)}
+        providerId={providerId}
+        clientId={selectedClientId}
+        handleBlockSlot={handleBlockSlot}
+        π
+        errorMessage={blockSlotError}
+        isCtaDisabled={isBlockSlotSubmitting}
+      />
     </Page>
   );
 };
