46baf76a8599e26a7c6d77a02575021b3924f370
Georgi 7DIGIT <georgi@7digit.io>
2022-11-27 00:14:10 +0200

    Create: useGetAllConsultationsByFilter hook

---
+++ src/hooks/useGetAllConsultationsByFilter.js
@@ -0,0 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { providerSvc } from "@USupport-components-library/services";
+
+export default function useGetAllConsultationsByFilter(filter) {
+  const fetchAllConsultations = async () => {
+    let response;
+    if (filter === "upcoming") {
+      response = await providerSvc.getAllUpcomingConsultations();
+    } else {
+      response = await providerSvc.getAllPastConsultations();
+    }
+    const data = response.data;
+    return data?.map((consultation) => ({
+      consultationId: consultation.consultation_id,
+      clientDetailId: consultation.client_detail_id,
+      clientName: consultation.client_name,
+      image: consultation.client_image,
+      timestamp: consultation.time,
+      status: consultation.status,
+    }));
+  };
+  const query = useQuery(["all-consultations", filter], fetchAllConsultations);
+
+  return query;
+}
+
+export { useGetAllConsultationsByFilter };
