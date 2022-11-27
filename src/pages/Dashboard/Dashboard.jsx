2191d840f78e5ee8284405da8794fe5d70eeefc1
Georgi 7DIGIT <georgi@7digit.io>
2022-11-28 01:29:07 +0200

    Create: Dashboard page

---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Page, Dashboard as DashboardBlock } from "#blocks";
+import { CancelConsultation, JoinConsultation } from "#backdrops";
+
+import "./dashboard.scss";
+import { useQueryClient } from "@tanstack/react-query";
+
+/**
+ * Dashboard
+ *
+ * Provider dashboard page
+ *
+ * @returns {JSX.Element}
+ */
+export const Dashboard = () => {
+  const queryClient = useQueryClient();
+  const [isCancelConsultationOpen, setIsCancelConsultationOpen] =
+    useState(false);
+  const openCancelConsultation = (consultation) => {
+    setIsCancelConsultationOpen(true);
+    setSelectedConsultation(consultation);
+  };
+  const closeCancelConsultation = () => setIsCancelConsultationOpen(false);
+
+  const [isJoinConsultationOpen, setIsJoinConsultationOpen] = useState(false);
+  const openJoinConsultation = () => setIsJoinConsultationOpen(true);
+  const closeJoinConsultation = () => setIsJoinConsultationOpen(false);
+
+  const [selectedConsultation, setSelectedConsultation] = useState();
+
+  const handleCancellationSuccess = () => {
+    queryClient.invalidateQueries({ queryKey: ["calendar-data"] });
+    queryClient.invalidateQueries({ queryKey: ["consultations-single-day"] });
+  };
+
+  return (
+    <Page classes="page__dashboard" showGoBackArrow={false}>
+      <DashboardBlock
+        openJoinConsultation={openJoinConsultation}
+        openCancelConsultation={openCancelConsultation}
+      />
+
+      {selectedConsultation && (
+        <CancelConsultation
+          isOpen={isCancelConsultationOpen}
+          onClose={closeCancelConsultation}
+          consultation={selectedConsultation}
+          onSuccess={handleCancellationSuccess}
+        />
+      )}
+      <JoinConsultation
+        isOpen={isJoinConsultationOpen}
+        onClose={closeJoinConsultation}
+      />
+    </Page>
+  );
+};
