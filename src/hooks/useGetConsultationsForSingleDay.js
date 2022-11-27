eda3907c3939dfd4f992f979221e8158f89c267a
Georgi 7DIGIT <georgi@7digit.io>
2022-11-28 01:25:33 +0200

    Create: useGetConsultationsForSingleDay

---
+++ src/hooks/useGetConsultationsForSingleDay.js
@@ -0,0 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import { providerSvc } from "@USupport-components-library/services";
+
+export const useGetConsultationsForSingleDay = (date) => {
+  const getconsultationsForSingleDay = async () => {
+    const { data } = await providerSvc.getConsultationsForSingleDay(date);
+    const filtered = data.filter((consultation) => {
+      if (
+        new Date(consultation.time).toLocaleDateString() !==
+        new Date(date * 1000).toLocaleDateString()
+      ) {
+        return false;
+      }
+      return true;
+    });
+    return filtered?.map((consultation) => ({
+      consultationId: consultation.consultation_id,
+      clientDetailId: consultation.client_detail_id,
+      clientName: consultation.client_name,
+      image: consultation.client_image,
+      timestamp: new Date(consultation.time).getTime(),
+      status: consultation.status,
+    }));
+  };
+
+  const query = useQuery(
+    ["consultations-single-day", date],
+    getconsultationsForSingleDay
+  );
+
+  return query;
+};
