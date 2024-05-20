e37a5a7b68689b316a529e231e504501e7d224a2
Georgi Ganchev @7DIGIT <georgi.ganchev@7digit.io>
2024-05-20 13:52:17 +0300

    Fix: polish translations file name

--- src/pages/Scheduler/locales.js
+++ src/pages/Scheduler/locales.js
@@ -1,3 +1,3 @@
 export * as en from "./locales/en.json";
-export * as uk from "./locales/uk.json"
-export * as po from "./locales/po.json"
+export * as uk from "./locales/uk.json";
+export * as pl from "./locales/pl.json";
--- src/routes/Root/locales.js
+++ src/routes/Root/locales.js
@@ -2,4 +2,4 @@ export * as en from "./locales/en.json";
 export * as ru from "./locales/ru.json";
 export * as kk from "./locales/kk.json";
 export * as uk from "./locales/uk.json";
-export * as po from "./locales/po.json";
+export * as pl from "./locales/pl.json";
