d0e73ff69cb253c056798928a483473fad6e74cb
7Digit-Main <it-support@7digit.io>
2026-06-16 14:39:16 +0300

    Fix: provider to get all tags

--- src/hooks/useGetQuestionsTags.js
+++ src/hooks/useGetQuestionsTags.js
@@ -2,13 +2,8 @@ import { useQuery } from "@tanstack/react-query";
 import { providerSvc } from "@USupport-components-library/services";
 
 export function useGetQuestionsTags(onSuccess) {
-  /**
-   *
-   *  @returns
-   */
-
   const getQuestionsTags = async () => {
-    const { data } = await providerSvc.getQuestionTags();
+    const { data } = await providerSvc.getQuestionTags("all");
     return data.map((item) => {
       return { label: item.tag, id: item.tag_id };
     });
